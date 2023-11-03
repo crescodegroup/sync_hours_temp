7516c78f40b6d30188da3a2a5322777129fc2b54
Crescode Group BV <info@crescode.eu>
2023-11-03 01:11:14 +0100

    add ClockifyClient for time entry retrieval or submission

---
+++ src/Service/Client/ClockifyClient.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Client;
+
+use Symfony\Component\HttpClient\DecoratorTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class ClockifyClient implements HttpClientInterface
+{
+    use DecoratorTrait;
+
+    public function __construct(private HttpClientInterface $clockifyClient)
+    {
+        $this->client = $clockifyClient;
+    }
+
+    public function __invoke()
+    {
+        $this->getUser();
+    }
+
+    public function getUser(): ResponseInterface
+    {
+        return $this->request(Request::METHOD_GET, 'user');
+    }
+
+    public function getTimeEntries(string $workspaceId, string $userId, array $queryParams = []): ResponseInterface
+    {
+        return $this->request(
+            Request::METHOD_GET,
+            sprintf('workspaces/%s/user/%s/time-entries', $workspaceId, $userId),
+            $queryParams
+        );
+    }
+}
