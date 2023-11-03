b7c17d7988dd94160251faf3aefa2f09b38fd2ea
Crescode Group BV <info@crescode.eu>
2023-11-03 01:13:39 +0100

    use controller for testing service invoke

--- src/Controller/Keeping/Sync/ClockifyController.php
+++ src/Controller/Keeping/Sync/ClockifyController.php
@@ -3,20 +3,23 @@
 namespace App\Controller\Keeping\Sync;
 
 use App\Service\KeepingService;
+use App\Service\SyncService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ClockifyController extends AbstractController
 {
-    public function __construct(private KeepingService $keepingService, private HttpClientInterface $clockifyClient)
+    public function __construct(private KeepingService $keepingService, private SyncService $syncService)
     {
     }
 
     #[Route('/keeping/sync/clockify', name: 'app_keeping_sync_clockify', format: 'json')]
     public function index(): JsonResponse
     {
+        //still in testing fase
+        $this->syncService->__invoke();
+
         return $this->json($this->keepingService->__invoke());
     }
 }
