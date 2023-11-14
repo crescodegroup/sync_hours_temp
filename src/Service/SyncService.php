198c8e806b0d246b98d04fed807cbf4b9d13eb6b
Crescode Group BV <info@crescode.eu>
2023-11-14 09:56:07 +0100

    WIP SyncService

--- src/Service/SyncService.php
+++ src/Service/SyncService.php
@@ -4,64 +4,28 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use App\Dto\Clockify\TimeEntry\TimeEntryDto;
-use App\Dto\Clockify\User\UserDto;
-use App\Service\Client\ClockifyClient;
-use DateTimeImmutable;
-use DateTimeZone;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class SyncService
 {
-    private ?UserDto $clockifyUser = null;
-
     public function __construct(
         private KeepingService $keepingService,
-        private ClockifyClient $clockifyClient,
+        private ClockifyService $clockifyService,
         private SerializerInterface $serializer,
     ) {
     }
 
     public function __invoke()
     {
-        $this->getClockifyUser();
-        $result = $this->getClockifyTimeEntriesPreviousMonth();
 
-        dd($this->clockifyUser, $result);
+        $entries = $this->clockifyService->getTimeEntriesPreviousMonth();
     }
 
-    private function getClockifyUser(): UserDto
-    {
-        if ($this->clockifyUser !== null) {
-            return $this->clockifyUser;
-        }
+    // create new client in keeping
+    // sync projects from clockify to keeping ECHC
+        // store projectID as keeping project code
+    // sync time-entries from clockify to keeping
 
-        $response = $this->clockifyClient->getUser();
-
-        $this->clockifyUser = $this->serializer->deserialize($response->getContent(), UserDto::class, 'json');
-
-        return $this->clockifyUser;
-    }
-
-    /** @return TimeEntryDto[] */
-    private function getClockifyTimeEntriesPreviousMonth(): array
-    {
-        $now = new DateTimeImmutable('now', new DateTimeZone('UTC'));
-        $start = $now->modify('first day of last month midnight');
-        $end = $now->modify('last day of this month midnight');
-
-        $response = $this->clockifyClient->getTimeEntries(
-            $this->clockifyUser->activeWorkspace,
-            $this->clockifyUser->id,
-            [
-                'query' => [
-                    'page-size' => '5000',
-                    'start' => $start->format('Y-m-d\TH:m:s.vp'),
-                    'end' => $end->format('Y-m-d\TH:m:s.vp')
-                ]
-            ]
-        );
-
-        return $this->serializer->deserialize($response->getContent(), TimeEntryDto::class . '[]', 'json');
-    }
+    // fetch time-entries from keeping
+    // insert into clockify in correct project
 }
